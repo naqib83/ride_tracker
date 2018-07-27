602bf9d9b3b22ec13fa990b4663a4b78e059a256
naqib83 <naqib83@gmail.com>
2018-07-27 11:56:57 -0500

    intemediate

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,10 +1,16 @@
 package com.pluralsight.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
@@ -17,8 +23,29 @@ public class RideRepositoryImpl implements RideRepository {
 	
 	@Override
 	public Ride createRide(Ride ride){
-		jdbcTemplate.update("insert into ride (name, duration) values(?, ?)", ride.getName(), ride.getDuration());
-		return null;
+		//jdbcTemplate.update("insert into ride (name, duration) values(?, ?)", ride.getName(), ride.getDuration());
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement ps = con.prepareStatement("insert into ride (name, duration) values(?, ?)", new String [] {"id"});
+				ps.setString(1, ride.getName());
+				ps.setInt(2, ride.getDuration());
+				
+				return ps;
+			}
+		}, keyHolder);
+		
+		Number id = keyHolder.getKey();
+		
+		return getRide(id.intValue());
+	}
+	
+	public Ride getRide(Integer id){
+		Ride ride = jdbcTemplate.queryForObject("select * from ride where id = ?", new RideRowMapper(), id);
+		
+		return ride;
 	}
 	
 	@Override
